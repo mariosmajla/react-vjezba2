cf5605c4046c3f76bc88af8edb5950b21dfe89b6
Matej Pavic <mpavic.dev@gmail.com>
2023-03-07 18:41:07 +0100

    dodana nova komponenta

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import PrvaKompa from './components/PrvaKompa';
+import DrugaKompa from './components/DrugaKompa';
 
 function App() {
   return (
     <div className="App">
       <h1>Algebra</h1>
       <PrvaKompa />
+      <DrugaKompa />
     </div>
   );
 }
