0fb9bed2435bee6772544fe918d85a59d2f8c6bf
Matej Pavic <mpavic.dev@gmail.com>
2023-03-07 18:28:14 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import PrvaKompa from './components/PrvaKompa';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Algebra</h1>
+      <PrvaKompa />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PrvaKompa.jsx
@@ -0,0 +1,9 @@
+const PrvaKompa = () => {
+  return (
+    <div>
+      <h2>Prva komponenta</h2>
+    </div>
+  );
+};
+
+export default PrvaKompa;
